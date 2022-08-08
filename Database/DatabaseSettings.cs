c07fb3f1dcaf881a4809fe5595b9d3d645274125
Domenico Valentino <domenico2727@gmail.com>
2022-08-07 21:45:42 -0400

    Fully working API

--- Database/DatabaseSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace Focal.Database
-{
-    public class DatabaseSettings : IDatabaseSettings
-    {
-        public string CollectionName { get; set; }
-        public string ConnectionString { get; set; }
-        public string DatabaseName { get; set; }
-    }
-}
--- Database/IDatabaseSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace Focal.Database
-{
-    public interface IDatabaseSettings
-    {
-        string CollectionName { get; set; }
-        string ConnectionString { get; set; }
-        string DatabaseName { get; set; }
-    }
-}
